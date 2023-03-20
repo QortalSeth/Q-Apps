88edfb0cec25a698e0ad722d1717c46de3b4ec2d
Phillip Lang Martinez <philliplangmartinez@gmail.com>
2023-03-20 17:22:20 +0200

    fix styling

--- examples/q-blog/src/pages/BlogIndividualPost/BlogIndividualPost.tsx
+++ examples/q-blog/src/pages/BlogIndividualPost/BlogIndividualPost.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
-import { Button } from '@mui/material';
+import { Button, Box, Typography, CardHeader, Avatar, } from '@mui/material';
 import { useNavigate } from "react-router-dom";
+import { styled } from '@mui/system';
 
 import ReadOnlySlate from '../../components/editor/ReadOnlySlate';
 import { useSelector } from 'react-redux';
@@ -9,10 +10,17 @@ import { RootState } from '../../state/store';
 import { checkStructure } from '../../utils/checkStructure';
 import { BlogContent } from '../../interfaces/interfaces';
 
-
+const EllipsisTypography = styled(Typography)`
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  width: 100%;
+`;
 export const BlogIndividualPost = () => {
   const { user, postId, blog } = useParams();
   const { user: userState } = useSelector((state: RootState) => state.auth);
+  const [avatarUrl, setAvatarUrl] = React.useState<string>('')
+
   const navigate = useNavigate();
 
    const [blogContent, setBlogContent] = React.useState<BlogContent | null>(null)
@@ -40,24 +48,71 @@ export const BlogIndividualPost = () => {
       getBlogPost()
     }, [])
 
+    const getAvatar = React.useCallback(async ()=> {
+      try {
+        let url = await qortalRequest({
+          action: "GET_QDN_RESOURCE_URL",
+          name: user,
+          service: "THUMBNAIL",
+          identifier: "qortal_avatar"
+        });
+       
+        console.log({url})
+        setAvatarUrl(url)
+      } catch (error) {
+        
+      }
+    }, [user])
+    React.useEffect(()=> {
+      getAvatar()
+    }, [])
+
     if(!blogContent) return null
+
+
   
     return (
-      <div className="App">
+      <Box sx={{
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column'
+      }}>
+        <Box sx={{
+          maxWidth: '700px',
+          margin: '15px'
+        }}>
         {user === userState?.name && (
           <Button onClick={()=> {
             navigate(`/${user}/${blog}/${postId}/edit`)
           }}>Edit Post</Button>
         )}
-        <p>{blogContent.title}</p>
+           <CardHeader
+        sx={{
+          '& .MuiCardHeader-content': {
+            overflow: 'hidden'
+          },
+         padding: '10px 0px'
+        }}
+        avatar={<Avatar src={avatarUrl} alt={`${user}'s avatar`} />}
+     
+        subheader={`Author: ${user}`}
+      />
+        <Typography variant="h1" color="textPrimary" sx={{
+          textAlign: 'center'
+        }}>
+        {blogContent.title}
+        </Typography>
+     
+ 
       {blogContent?.postContent?.map((section: any)=> {
         if(section.type === 'editor'){
           return <ReadOnlySlate key={section.id} content={section.content}  />
         }
         if(section.type === 'image'){
-          return <img src={section.content.image}  />
+          return <img src={section.content.image} className="post-image" />
         }
        })}
-      </div>
+      </Box>
+      </Box>
     );
 }
