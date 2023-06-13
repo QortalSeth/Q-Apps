d3fd0420fb63ef3a79dfc4eadb1b72504ad3623e
Phillip Lang Martinez <philliplangmartinez@gmail.com>
2023-06-13 07:04:37 +0300

    added html content to qmail

--- examples/q-mail/src/utils/checkStructure.ts
+++ examples/q-mail/src/utils/checkStructure.ts
@@ -1,49 +1,48 @@
 // check the structure of blog posts
-export const checkStructure = (content: any)=> {
-    let isValid = true
-    if(!content?.title) isValid = false
-    if(!content?.createdAt) isValid = false
-    if(!Array.isArray(content?.postContent)) isValid = false
-    content?.postContent?.forEach((c: any)=> {
-      if (!c.type) {
-        isValid = false;
-      }
-      if (!c.version) {
-        isValid = false;
-      }
-      if (!c.id) {
-        isValid = false;
-      }
-      if (!c.content) {
-        isValid = false;
-      }
-      if (
-        c.version === 1 &&
-        c.type !== 'editor' &&
-        c.type !== 'image' &&
-        c.type !== 'video' &&
-        c.type !== 'audio' &&
-        c.type !== 'file'
-      ) {
-        isValid = false
-      }
-    });
-  
-    return isValid
-  }
+export const checkStructure = (content: any) => {
+  let isValid = true
+  if (!content?.title) isValid = false
+  if (!content?.createdAt) isValid = false
+  if (!Array.isArray(content?.postContent)) isValid = false
+  content?.postContent?.forEach((c: any) => {
+    if (!c.type) {
+      isValid = false;
+    }
+    if (!c.version) {
+      isValid = false;
+    }
+    if (!c.id) {
+      isValid = false;
+    }
+    if (!c.content) {
+      isValid = false;
+    }
+    if (
+      c.version === 1 &&
+      c.type !== 'editor' &&
+      c.type !== 'image' &&
+      c.type !== 'video' &&
+      c.type !== 'audio' &&
+      c.type !== 'file'
+    ) {
+      isValid = false
+    }
+  });
 
-  export const checkStructureMailMessages = (content: any) => {
-    let isValid = true
+  return isValid
+}
 
-    // if (!content?.title) isValid = false
-    // if (!content?.description) isValid = false
-    if (!content?.createdAt) isValid = false
-    if (!content?.version) isValid = false
-    if (!Array.isArray(content?.attachments)) isValid = false
-    if (!Array.isArray(content?.textContent)) isValid = false
-    if (typeof content?.generalData !== 'object') isValid = false
+export const checkStructureMailMessages = (content: any) => {
+  let isValid = true
 
-    return isValid
-  }
+  // if (!content?.title) isValid = false
+  // if (!content?.description) isValid = false
+  if (!content?.createdAt) isValid = false
+  if (!content?.version) isValid = false
+  if (!Array.isArray(content?.attachments)) isValid = false
+  if (!content?.htmlContent && !Array.isArray(content?.textContent)) isValid = false
+  if (typeof content?.generalData !== 'object') isValid = false
 
-  
+  return isValid
+}
+
