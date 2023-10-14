55317e22bd17e2ef18ced69c2a8144272ae1e8a6
Justin Ferrari <justinwesleyferrari@gmail.com>
2023-10-14 10:44:25 -0500

    Q-Shop Bug Fixes

--- examples/q-shop/src/constants/identifiers.ts
+++ examples/q-shop/src/constants/identifiers.ts
@@ -1,12 +1,14 @@
 // TESTING
-export const TEST_STORE_BASE = "test-q-store-general";
-export const TEST_DATA_CONTAINER_BASE = "test-datacontainer";
-export const TEST_CATALOGUE_BASE = "test-q-store-catalogue";
-export const TEST_ORDER_BASE = "test-q-store-order";
-export const TEST_REVIEW_BASE = "test-q-store-review";
-export const TEST_PRODUCT_BASE = "test-q-store-product";
+
+// export const STORE_BASE = "test-q-store-general";
+// export const DATA_CONTAINER_BASE = "test-datacontainer";
+// export const CATALOGUE_BASE = "test-q-store-catalogue";
+// export const ORDER_BASE = "test-q-store-order";
+// export const REVIEW_BASE = "test-q-store-review";
+// export const PRODUCT_BASE = "test-q-store-product";
 
 // PRODUCTION
+
 export const STORE_BASE = "q-store-general";
 export const DATA_CONTAINER_BASE = "datacontainer";
 export const CATALOGUE_BASE = "q-store-catalogue";
