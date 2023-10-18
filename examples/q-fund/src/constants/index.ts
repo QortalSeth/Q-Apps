195c8f0af174febbe029fa78f75c135ac41e35b0
IrohDW <randomprogrammer@protonmail.com>
2023-10-18 13:13:07 -0600

    Bugfix - Displayed QORT raised by Q-Funds are rounded to nearest whole number
    This bugfix prevents the number overflowing past progress bar and being cut off with ....
    
    Library section removed from root dir in repository because it will soon be added to its own repository. Latest Library dir is still present in examples/q-fund/src/utils
    
    SendCoin.ts added to Q-Fund Library. This enables sending any supported coin easily in a Q-App through simple function calls.
    
    DonorInfo.tsx aggregates donors with multiple donations by default.
    
    Bug in BoundedNumericTextField.tsx involving the listeners on its IncDec buttons fixed. Textfield in Donate.tsx now uses the IncDec buttons.
    
    Updates to buttons in Donate.tsx:
     1. Donate close modal button variant changed to contained. This makes it easier to read its text while being consistent with other buttons on the crowdfund page. (Same was done to cancel button on NewCrowdfund.tsx)
    
     2. Send Coin button text is set to white always so it is consistent with other buttons on the crowdfund page in both light and dark modes.
    
     README updated to provide more details about the nature of Q-Apps to people who haven't heard of the concept.
    
     Removed Publish-Limit-Breaker since it is no longer being developed due to there being better options for larger uploads.
    
     Q-Fund uses new npm library qortal-apps-utils instead of storing Library files in Utils directory.
    
     DonorModal now updates donor data every 30 seconds

--- examples/q-fund/src/constants/index.ts
+++ examples/q-fund/src/constants/index.ts
@@ -1,13 +1,22 @@
+const useTestIdentifiers = false;
 
-export const CROWDFUND_BASE = 'q-fund_crowdfund_'
+export const CROWDFUND_BASE = useTestIdentifiers
+  ? "MYTEST_crowdfund_"
+  : "q-fund_crowdfund_";
 
-export const ATTACHMENT_BASE = 'attachments_q-fund_'
+export const ATTACHMENT_BASE = useTestIdentifiers
+  ? "attachments_MYTEST_"
+  : "attachments_q-fund_";
 
-export const COMMENT_BASE = 'qcomment_v1_q-fund_'
+export const COMMENT_BASE = useTestIdentifiers
+  ? "qcomment_v1_MYTEST_"
+  : "qcomment_v1_q-fund_";
 
-export const UPDATE_BASE = 'q-fund_update_crowdfund_'
+export const UPDATE_BASE = useTestIdentifiers
+  ? "MYTEST_update_crowdfund_"
+  : "q-fund_update_crowdfund_";
 
-export const REVIEW_BASE = "q-fund-rw"
+export const REVIEW_BASE = useTestIdentifiers ? "q-fund-testrw" : "q-fund-rw";
 
 // export const CROWDFUND_BASE = 'MYTEST_crowdfund_'
 
@@ -18,4 +27,3 @@ export const REVIEW_BASE = "q-fund-rw"
 // export const UPDATE_BASE = 'MYTEST_update_crowdfund_'
 
 // export const REVIEW_BASE = "q-fund-testrw"
-
