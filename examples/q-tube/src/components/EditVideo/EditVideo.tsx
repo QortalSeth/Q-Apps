991c45ba3021635b86641f1e9dad069d4515f435
PhilReact <philliplangmartinez@gmail.com>
2023-12-03 18:28:28 +0200

    version 1 qtube

---
+++ examples/q-tube/src/components/EditVideo/EditVideo.tsx
@@ -0,0 +1,448 @@
+import React, { useEffect, useState } from "react";
+import {
+  AddCoverImageButton,
+  AddLogoIcon,
+  CoverImagePreview,
+  CrowdfundActionButton,
+  CrowdfundActionButtonRow,
+  CustomInputField,
+  CustomSelect,
+  LogoPreviewRow,
+  ModalBody,
+  NewCrowdfundTitle,
+  StyledButton,
+  TimesIcon,
+} from "./Upload-styles";
+import {
+  Box,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Modal,
+  OutlinedInput,
+  Select,
+  SelectChangeEvent,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import ShortUniqueId from "short-unique-id";
+import { useDispatch, useSelector } from "react-redux";
+import AddBoxIcon from "@mui/icons-material/AddBox";
+import { useDropzone } from "react-dropzone";
+
+import { setNotification } from "../../state/features/notificationsSlice";
+import { objectToBase64, uint8ArrayToBase64 } from "../../utils/toBase64";
+import { RootState } from "../../state/store";
+import {
+  upsertVideosBeginning,
+  addToHashMap,
+  upsertVideos,
+  setEditVideo,
+  updateVideo,
+  updateInHashMap,
+} from "../../state/features/videoSlice";
+import ImageUploader from "../common/ImageUploader";
+import { categories, subCategories } from "../../constants";
+
+const uid = new ShortUniqueId();
+const shortuid = new ShortUniqueId({ length: 5 });
+
+interface NewCrowdfundProps {
+  editId?: string;
+  editContent?: null | {
+    title: string;
+    user: string;
+    coverImage: string | null;
+  };
+}
+
+interface VideoFile {
+  file: File;
+  title: string;
+  description: string;
+  coverImage?: string;
+}
+export const EditVideo = () => {
+  const theme = useTheme();
+  const dispatch = useDispatch();
+  const username = useSelector((state: RootState) => state.auth?.user?.name);
+  const userAddress = useSelector(
+    (state: RootState) => state.auth?.user?.address
+  );
+  const editVideoProperties = useSelector(
+    (state: RootState) => state.video.editVideoProperties
+  );
+
+  const [title, setTitle] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const [coverImage, setCoverImage] = useState<string>("");
+
+  const [selectedCategoryVideos, setSelectedCategoryVideos] =
+    useState<any>(null);
+  const [selectedSubCategoryVideos, setSelectedSubCategoryVideos] =
+    useState<any>(null);
+
+  // useEffect(() => {
+  //   if (editVideoProperties) {
+  //     const descriptionString = editVideoProperties?.description || "";
+  //     // Splitting the string at the asterisks
+  //     const parts = descriptionString.split("**");
+
+  //     // The part within the asterisks
+  //     const extractedString = parts[1];
+
+  //     // The part after the last asterisks
+  //     const description = parts[2] || ""; // Using '|| '' to handle cases where there is no text after the last **
+  //     setTitle(editVideoProperties?.title || "");
+  //     setDescription(editVideoProperties?.fullDescription || "");
+  //     setCoverImage(editVideoProperties?.videoImage || "");
+
+  //     // Split the extracted string into key-value pairs
+  //     const keyValuePairs = extractedString.split(";");
+
+  //     // Initialize variables to hold the category and subcategory values
+  //     let category, subcategory;
+
+  //     // Loop through each key-value pair
+  //     keyValuePairs.forEach((pair) => {
+  //       const [key, value] = pair.split(":");
+
+  //       // Check the key and assign the value to the appropriate variable
+  //       if (key === "category") {
+  //         category = value;
+  //       } else if (key === "subcategory") {
+  //         subcategory = value;
+  //       }
+  //     });
+
+  //     if(category){
+  //       const selectedOption = categories.find((option) => option.id === +category);
+  //   setSelectedCategoryVideos(selectedOption || null);
+  //     }
+
+  //     if(subcategory){
+  //       const selectedOption = categories.find((option) => option.id === +subcategory);
+  //   setSelectedCategoryVideos(selectedOption || null);
+  //     }
+
+  //   }
+  // }, [editVideoProperties]);
+
+  useEffect(() => {
+    if (editVideoProperties) {
+      setTitle(editVideoProperties?.title || "");
+      setDescription(editVideoProperties?.fullDescription || "");
+      setCoverImage(editVideoProperties?.videoImage || "");
+
+      if (editVideoProperties?.category) {
+        const selectedOption = categories.find(
+          (option) => option.id === +editVideoProperties.category
+        );
+        setSelectedCategoryVideos(selectedOption || null);
+      }
+
+      if (
+        editVideoProperties?.category &&
+        editVideoProperties?.subcategory &&
+        subCategories[+editVideoProperties?.category]
+      ) {
+        const selectedOption = subCategories[
+          +editVideoProperties?.category
+        ]?.find((option) => option.id === +editVideoProperties.subcategory);
+        setSelectedSubCategoryVideos(selectedOption || null);
+      }
+    }
+  }, [editVideoProperties]);
+
+  const onClose = () => {
+    dispatch(setEditVideo(null));
+  };
+
+  async function publishQDNResource() {
+    try {
+      if(!title) throw new Error('Please enter a title')
+      if(!description) throw new Error('Please enter a description')
+      if(!coverImage) throw new Error('Please select cover image')
+      if(!selectedCategoryVideos) throw new Error('Please select a category')
+      if (!editVideoProperties) return;
+      if (!userAddress) throw new Error("Unable to locate user address");
+      let errorMsg = "";
+      let name = "";
+      if (username) {
+        name = username;
+      }
+      if (!name) {
+        errorMsg =
+          "Cannot publish without access to your name. Please authenticate.";
+      }
+
+      if (editVideoProperties?.user !== username) {
+        errorMsg = "Cannot publish another user's resource";
+      }
+
+      if (errorMsg) {
+        dispatch(
+          setNotification({
+            msg: errorMsg,
+            alertType: "error",
+          })
+        );
+        return;
+      }
+      const category = selectedCategoryVideos.id;
+      const subcategory = selectedSubCategoryVideos?.id || "";
+
+      const videoObject: any = {
+        title,
+        version: editVideoProperties.version,
+        fullDescription: description,
+        videoImage: coverImage,
+        videoReference: editVideoProperties.videoReference,
+        commentsId: editVideoProperties.commentsId,
+        category,
+        subcategory,
+        code: editVideoProperties.code,
+      };
+
+      let metadescription =
+        `**category:${category};subcategory:${subcategory};code:${editVideoProperties.code}**` +
+        description.slice(0, 150);
+
+      const crowdfundObjectToBase64 = await objectToBase64(videoObject);
+      // Description is obtained from raw data
+      const requestBodyJson: any = {
+        action: "PUBLISH_QDN_RESOURCE",
+        name: username,
+        service: "DOCUMENT",
+        data64: crowdfundObjectToBase64,
+        title: title.slice(0, 50),
+        description: metadescription,
+        identifier: editVideoProperties.id,
+        tag1: "qtube_vid_",
+      };
+
+      await qortalRequest(requestBodyJson);
+
+      dispatch(
+        updateVideo({
+          ...editVideoProperties,
+          ...videoObject,
+        })
+      );
+      dispatch(
+        updateInHashMap({
+          ...editVideoProperties,
+          ...videoObject,
+        })
+      );
+
+      onClose();
+    } catch (error: any) {
+      let notificationObj: any = null;
+      if (typeof error === "string") {
+        notificationObj = {
+          msg: error || "Failed to publish update",
+          alertType: "error",
+        };
+      } else if (typeof error?.error === "string") {
+        notificationObj = {
+          msg: error?.error || "Failed to publish update",
+          alertType: "error",
+        };
+      } else {
+        notificationObj = {
+          msg: error?.message || "Failed to publish update",
+          alertType: "error",
+        };
+      }
+      if (!notificationObj) return;
+      dispatch(setNotification(notificationObj));
+
+      throw new Error("Failed to publish update");
+    }
+  }
+
+  const handleOnchange = (index: number, type: string, value: string) => {
+    // setFiles((prev) => {
+    //   let formattedValue = value
+    //   console.log({type})
+    //   if(type === 'title'){
+    //     formattedValue = value.replace(/[^a-zA-Z0-9\s]/g, "")
+    //   }
+    //   const copyFiles = [...prev];
+    //   copyFiles[index] = {
+    //     ...copyFiles[index],
+    //     [type]: formattedValue,
+    //   };
+    //   return copyFiles;
+    // });
+  };
+
+  const handleOptionCategoryChangeVideos = (
+    event: SelectChangeEvent<string>
+  ) => {
+    const optionId = event.target.value;
+    const selectedOption = categories.find((option) => option.id === +optionId);
+    setSelectedCategoryVideos(selectedOption || null);
+  };
+  const handleOptionSubCategoryChangeVideos = (
+    event: SelectChangeEvent<string>,
+    subcategories: any[]
+  ) => {
+    const optionId = event.target.value;
+    const selectedOption = subcategories.find(
+      (option) => option.id === +optionId
+    );
+    setSelectedSubCategoryVideos(selectedOption || null);
+  };
+
+
+  return (
+    <>
+      <Modal
+        open={!!editVideoProperties}
+        aria-labelledby="modal-title"
+        aria-describedby="modal-description"
+      >
+        <ModalBody>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <NewCrowdfundTitle>Update Video properties</NewCrowdfundTitle>
+          </Box>
+          <>
+            <Box
+              sx={{
+                display: "flex",
+                gap: "20px",
+                alignItems: "center",
+              }}
+            >
+              <FormControl fullWidth sx={{ marginBottom: 2 }}>
+                <InputLabel id="Category">Select a Category</InputLabel>
+                <Select
+                  labelId="Category"
+                  input={<OutlinedInput label="Select a Category" />}
+                  value={selectedCategoryVideos?.id || ""}
+                  onChange={handleOptionCategoryChangeVideos}
+                >
+                  {categories.map((option) => (
+                    <MenuItem key={option.id} value={option.id}>
+                      {option.name}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+              {selectedCategoryVideos &&
+                subCategories[selectedCategoryVideos?.id] && (
+                  <FormControl fullWidth sx={{ marginBottom: 2 }}>
+                    <InputLabel id="Category">Select a Sub-Category</InputLabel>
+                    <Select
+                      labelId="Sub-Category"
+                      input={<OutlinedInput label="Select a Sub-Category" />}
+                      value={selectedSubCategoryVideos?.id || ""}
+                      onChange={(e) =>
+                        handleOptionSubCategoryChangeVideos(
+                          e,
+                          subCategories[selectedCategoryVideos?.id]
+                        )
+                      }
+                    >
+                      {subCategories[selectedCategoryVideos.id].map(
+                        (option) => (
+                          <MenuItem key={option.id} value={option.id}>
+                            {option.name}
+                          </MenuItem>
+                        )
+                      )}
+                    </Select>
+                  </FormControl>
+                )}
+            </Box>
+            <React.Fragment>
+              {!coverImage ? (
+                <ImageUploader onPick={(img: string) => setCoverImage(img)}>
+                  <AddCoverImageButton variant="contained">
+                    Add Cover Image
+                    <AddLogoIcon
+                      sx={{
+                        height: "25px",
+                        width: "auto",
+                      }}
+                    ></AddLogoIcon>
+                  </AddCoverImageButton>
+                </ImageUploader>
+              ) : (
+                <LogoPreviewRow>
+                  <CoverImagePreview src={coverImage} alt="logo" />
+                  <TimesIcon
+                    color={theme.palette.text.primary}
+                    onClickFunc={() => setCoverImage("")}
+                    height={"32"}
+                    width={"32"}
+                  ></TimesIcon>
+                </LogoPreviewRow>
+              )}
+              <CustomInputField
+                name="title"
+                label="Title of video"
+                variant="filled"
+                value={title}
+                onChange={(e) => {
+                  const value = e.target.value
+                  const formattedValue = value.replace(/[^a-zA-Z0-9\s]/g, "");
+                  setTitle(formattedValue);
+                }}
+                inputProps={{ maxLength: 180 }}
+                required
+              />
+              <CustomInputField
+                name="description"
+                label="Describe your video in a few words"
+                variant="filled"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                inputProps={{ maxLength: 180 }}
+                multiline
+                maxRows={3}
+                required
+              />
+            </React.Fragment>
+          </>
+
+          <CrowdfundActionButtonRow>
+            <CrowdfundActionButton
+              onClick={() => {
+                onClose();
+              }}
+              variant="contained"
+              color="error"
+            >
+              Cancel
+            </CrowdfundActionButton>
+            <Box
+              sx={{
+                display: "flex",
+                gap: "20px",
+                alignItems: "center",
+              }}
+            >
+              <CrowdfundActionButton
+                variant="contained"
+                onClick={() => {
+                  publishQDNResource();
+                }}
+              >
+                Publish
+              </CrowdfundActionButton>
+            </Box>
+          </CrowdfundActionButtonRow>
+        </ModalBody>
+      </Modal>
+    </>
+  );
+};
